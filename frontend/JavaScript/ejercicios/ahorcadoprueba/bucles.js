37afcb61992774e4f2c0e9aea51a70acd2334c39
dafnnee14 <srta.dafnnee14@gmail.com>
2022-11-11 13:18:07 +0100

    curso-desarrollo-web

--- frontend/JavaScript/ejercicios/ahorcadoprueba/bucles.js
+++ frontend/JavaScript/ejercicios/ahorcadoprueba/bucles.js
@@ -1,32 +1,101 @@
-const display = document.getElementById("display")
-const displayLetras = document.getElementById("displayLetras")
+const display = document.getElementById('display');
+const displayLetras = document.getElementById('displayLetras');
 
-let texto = "";
+let texto = '';
+let arrayNumeros = [];
 
-// Queremos imprimir todos los números del 1 al 20
-for (let i = 1; i <= 20; i++) {
-    texto += i + ", ";
+// Queremos imprimir todos los números pares del 1 al 20
+for (let i = 2; i < 20; i = i + 2) {
+    texto += i + " ";
+}
 
-
+for (let i = 0; i < 20; i = i + 2) {
+    arrayNumeros[i] = i;
 }
 
 display.innerHTML = texto;
-
-display.innerHTML = texto;
-// console.log(arrayNumeros)
+console.log(arrayNumeros)
 
 const letras = [
-"a",
-"e",
-"i",
-"o",
-"u"
+    "a",
+    "e",
+    "i",
+    "o",
+    "u"
 ]
 
-texto = "";
+texto = '';
 
 for (let i = 0; i < letras.length; i++) {
-    texto += letras[i] + " "; 
+    texto += letras[i] + " ";
+
 }
 
-displayLetras.innerHTML = texto;
+displayLetras.innerHTML = letras.join(' ');
+
+
+// LLenar un array con números pares
+let pares = [];
+
+for (let i = 1; i <= 10; i++) {
+    pares[i - 1] = i * 2;
+
+}
+
+console.log(pares);
+display.innerHTML = pares;
+
+
+// Imprimir los múltiplos del 3 hasta el 100
+texto = '';
+
+for (let i = 3; i < 100; i = i + 3) {
+    texto += i + ", "
+
+}
+
+display.innerHTML = texto;
+
+
+// Lo mismo pero cuenta atrás desde el 100
+texto = '';
+
+for (let i = 99; i >= 3; i = i - 3) {
+
+    texto += i + "<br>";
+}
+
+display.innerHTML = texto;
+// display.innerHTML = pares.join('<br>');
+// display.innerHTML = arrayNumeros;
+
+
+// Números pares hasta el 100, y que cada vez que 
+//  salga un múltiplo de 5 vaya en negrita
+
+let txt = "";
+
+for (let i = 2; i <= 100; i = i + 2) {
+
+    // Si se divide i entre 5
+}
+
+
+
+
+
+
+display.innerHTML = txt;
+
+// Cuenta atras en un array
+
+let countdown = [];
+
+for (let i = 0; i < arrayNumeros.length; i++) {
+    for (let j = 100; j >= 0 j--) {
+
+        countdown[i] = j;
+    }
+}
+
+display.innerHTML 0 countdown.join("<br>");
