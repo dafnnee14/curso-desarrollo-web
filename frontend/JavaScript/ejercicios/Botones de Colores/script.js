4952b61b678f1ddb26a62d15a21868924995b323
dafnnee14 <srta.dafnnee14@gmail.com>
2022-11-24 12:36:44 +0100

    curso-desarrollo-web

---
+++ frontend/JavaScript/ejercicios/Botones de Colores/script.js
@@ -0,0 +1,11 @@
+
+
+
+// Función sencilla
+function colorChange(color) {
+    document.body.style.background = color;
+    console.log("has pulsado un botón");
+}
+function colorReset() {
+    document.body.style.background = 'linear-gradient(90deg,rgb(41, 56, 97), black)';
+}
