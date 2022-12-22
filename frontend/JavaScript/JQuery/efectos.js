32a30c8e27b585032a76f4796d75db6e7fee8f41
dafnnee14 <srta.dafnnee14@gmail.com>
2022-12-22 12:20:01 +0100

    curso-desarrollo-web

---
+++ frontend/JavaScript/JQuery/efectos.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $('#btnHide').click(function () {
+        $('p').hide(1500);
+    })
+
+    $('#btnShow').click(function () {
+        $('p').show(3000);
+    })
+    $('#btnToggle').click(function () {
+        $('p').Toggle(500);
+    })
+    // Fade
+    $('#btnFadeIn').click(function () {
+        $('p').fadeIn();
+    });
+    $('#btnToggle').click(function () {
+        $('p').fadeOut();
+    });
+    $('#btnFadeToggle').click(function () {
+        $('p').fadeToggle('fast');
+    });
+    $('#btnFadeTo').click(function () {
+        $('p').fadeToggle('slow', 0.3);
+    });
+});
