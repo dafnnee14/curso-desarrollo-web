287e67aa2c3a127091651b636bac1212cbc02a13
dafnnee14 <srta.dafnnee14@gmail.com>
2023-02-02 13:07:17 +0100

    curso-desarrollol-web

---
+++ Backend/php/ejercicios/logeo y registro/Formulario-IncioDeSesion/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login y Registro</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <form action="login.php" method="post">
+        <h2>Login</h2>
+        <label>User Name</label>
+        <input type="text" name="uname" placeholder="User Name"><br>
+
+        <label>User Name</label>
+        <input type="password" name="password" placeholder="Password"><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+
+</html>
---
+++ Backend/php/ejercicios/logeo y registro/Formulario-IncioDeSesion/login.php
@@ -0,0 +1,29 @@
+<?php 
+
+if (isset($_POST['uname'])) && isset(($_POST['password'])) {
+ function validate($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $
+ }};
+ 
+ 
+ 
+$uname = validate($_POST['uname']);
+$uname = validate($_POST['password']);
+
+if (empty($uname)) {
+    header("Location: index.php?error=User Name is required");
+    exit();
+} else if (empty(pass)) {
+    header("Location: index.php?error=Password is required");
+}else {
+    echo "Valid input";
+} else {
+    header("Location: index.php");
+    exit();
+}
+
+
+
