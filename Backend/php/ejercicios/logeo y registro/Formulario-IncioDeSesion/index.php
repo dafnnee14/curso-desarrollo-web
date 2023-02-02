6dc7abb86718a7b98613713255bb57d710924fcf
dafnnee14 <srta.dafnnee14@gmail.com>
2023-02-02 13:43:37 +0100

    curso-desarrollo-web

--- Backend/php/ejercicios/logeo y registro/Formulario-IncioDeSesion/index.php
+++ Backend/php/ejercicios/logeo y registro/Formulario-IncioDeSesion/index.php
@@ -12,6 +12,9 @@
 <body>
     <form action="login.php" method="post">
         <h2>Login</h2>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
         <label>User Name</label>
         <input type="text" name="uname" placeholder="User Name"><br>
 
